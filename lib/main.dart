aa2fbdbe4c21cd27387a861f8169d8705a84eaef
Fernando Augusto <fernando.augustols@outlook.com>
2022-09-13 21:24:10 -0300

    micard com containers

--- lib/main.dart
+++ lib/main.dart
@@ -34,14 +34,62 @@ class _MyAppState extends State<MyApp> {
                 color: Colors.white,
                 fontWeight: FontWeight.bold),
           ),
-          Text('FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                color: Colors.teal.shade100,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ))
+          Text(
+            'FLUTTER DEVELOPER',
+            style: TextStyle(
+              fontFamily: 'Source Sans Pro',
+              color: Colors.teal.shade100,
+              fontSize: 20.0,
+              letterSpacing: 2.5,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(10.0),
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  '+55 11 97255-7700',
+                  style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro'),
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(10.0),
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'fernando.augustols@outlook.com',
+                  style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro'),
+                ),
+              ],
+            ),
+          )
         ],
       )),
     );
